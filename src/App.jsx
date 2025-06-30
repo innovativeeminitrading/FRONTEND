f4781e5f657ae94b94203876dc0877070ef6defa
innovativeeminitrading <fordhamrobert733@gmail.com>
2025-06-29 20:28:50 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>Welcome to Innovative Emini Trading</h1>
+      <p>This is your Vite + React setup.</p>
+    </div>
+  );
+}
+
+export default App;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
