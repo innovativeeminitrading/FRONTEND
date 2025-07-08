5d83c6b8aa8ef975427298395f545630824acf81
innovativeeminitrading <fordhamrobert733@gmail.com>
2025-07-07 22:18:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,4 @@ function App() {
 }
 
 export default App;
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
