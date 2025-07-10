08421aabaed851a5f9f54c700a97a0f8f7277bba
innovativeeminitrading <fordhamrobert733@gmail.com>
2025-07-10 10:18:31 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,1 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC-dtTCavEWJBpKps18sErmKknEv3zhPD4",
-  authDomain: "fir-innovativeemini.firebaseapp.com",
-  projectId: "fir-innovativeemini",
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth, signInWithEmailAndPassword };
