6f4b1722797dc8a25c99e040f8e6829a228438f0
innovativeeminitrading <fordhamrobert733@gmail.com>
2025-07-10 10:29:58 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,1 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
-      <h1>Welcome to Innovative Emini Trading</h1>
-      <p>This is your Vite + React setup.</p>
-    </div>
-  );
-}
-
-export default App;
 
