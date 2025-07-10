a2eb858289dc8a685e9427fcb78ac36b25aded8a
innovativeeminitrading <fordhamrobert733@gmail.com>
2025-07-09 22:16:21 -0400

    Update getToken.js

--- src/getToken.js
+++ src/getToken.js
@@ -1,1 +1,17 @@
+// src/getToken.js
+import { auth, signInWithEmailAndPassword } from './firebase';
 
+const email = "fordhamrobert733@gmail.com";
+const password = "Base_1924";
+
+signInWithEmailAndPassword(auth, email, password)
+  .then(userCredential => {
+    return userCredential.user.getIdToken();
+  })
+  .then(idToken => {
+    console.log("✅ Your Firebase ID Token:");
+    console.log(idToken);
+  })
+  .catch(error => {
+    console.error("❌ Error logging in:", error);
+  });
