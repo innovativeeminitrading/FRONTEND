66ccb0b2bb9458368e92d8be244dd005789eb30a
innovativeeminitrading <fordhamrobert733@gmail.com>
2025-07-10 10:30:32 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
 
+const App = () => {
+  return (
+    <Router>
+      <nav style={{ padding: '1rem', background: '#f0f0f0' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Login</Link>
+        <Link to="/signup">Signup</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
