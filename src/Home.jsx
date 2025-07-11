fccfe56bedb415d04416a02b4115be07dae0c2bf
innovativeeminitrading <fordhamrobert733@gmail.com>
2025-07-11 09:53:10 -0400

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
 
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Innovative Emini Trading</h1>
+      <p>This is the home page. Use the navigation menu to explore the app.</p>
+    </div>
+  );
+}
